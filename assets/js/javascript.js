8ded9eb9a4de724c423959009d5f9ce399d1a0b1
Koober97 <koober@Brians-MacBook-Pro.local>
2022-07-07 17:27:08 -0400

    created initial js code

---
+++ assets/js/javascript.js
@@ -0,0 +1,11 @@
+var timeCurrent = moment();
+
+// set time from 9am to 5pm
+const totalHour = 9;
+const startHour = 9;
+
+// add function to generate all the needed time rows
+function generateRows() {
+    
+    var o1El = $("<o1>");
+}
