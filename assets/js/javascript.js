aff68714474072f169e646293604a487a9403427
Koober97 <koober@Brians-MacBook-Pro.local>
2022-07-11 20:02:36 -0400

    start laying out table in js

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -8,4 +8,37 @@ const startHour = 9;
 function generateRows() {
     
     var o1El = $("<o1>");
+    o1El.attr("class", "time-block");
+    $('div.container').append(o1El);
+
+    for (var i = startHour; i < startHour + totalHour; i++) {
+        var liEl = $("<li>");
+        liEl.attr("class", "row");
+        $('ol').append(liEl);
+
+        var str = "";
+        var h = i % 24;
+        if (h < 12) {
+            str = h + "AM";
+        } else if (h == 12) {
+            str = h + "PM";
+        } else {
+            str = (h - 12) + "PM";
+        };
+
+        var label = $("<label>");
+        label.attr("class", "col-1 hour py-3");
+        label.text(str);
+
+        var inputEl = $("<textarea>");
+        inputEl.attr({
+            'class': "col-10",
+            'id': str,
+            'data-begin': h,
+        });
+    }
 }
+
+// $('.savedBtn').on('click', function () {
+//     var value = $(this).sibling
+// }
